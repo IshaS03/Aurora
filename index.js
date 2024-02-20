58f5662a06e3cf47fc0dbf94f91dc6e2e32f272b
Isha Sharma <121427020+IshaS03@users.noreply.github.com>
2024-02-20 21:44:41 +0530

    home page, shop and profile page
    home page, shop and profile page is almost done, still working on cart

---
+++ index.js
@@ -0,0 +1,10 @@
+<div class="shop-header">
+            <i class="fas fa-filter">Show Filters</i>
+            <div class="input-group rounded">
+                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+                    aria-describedby="search-addon" />
+                <span class="input-group-text border-0" id="search-addon">
+                    <i class="fas fa-search"></i>
+                </span>
+            </div>
+        </div>
